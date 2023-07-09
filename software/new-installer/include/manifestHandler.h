3c859bbd4cd6d3cbbba9124c8b2642a02f82bb92
Karl-Heinrich Tamm <karl.heinrich@zoho.com>
2023-07-09 21:20:01 +0300

    check

--- software/new-installer/include/manifestHandler.h
+++ software/new-installer/include/manifestHandler.h
@@ -7,11 +7,16 @@
 #include <QJsonObject>
 #include <QString>
 
+#define EXTENSION_NAME       "easyPlay"
+#define EXTENSION_ID         "easyPlay@easyPlay"
+#define MANIFEST_FILE_NAME   EXTENSION_NAME ".json"
+#define MANIFEST_DESCRIPTION "EasyPlay Device Client"
+
 // TODO: remove #Includes that are not needed
 
 class ManifestHandler {
  public:
-  static void createManifest(const QString& directoryPath, const QString& clientExePath) {
+  static bool createManifest(const QString& directoryPath, const QString& clientExePath) {
     // TODO: add to cpp file
     // TODO: check that directory is real
 
@@ -20,20 +25,23 @@ class ManifestHandler {
 
     if (!manifestFile.open(QFile::WriteOnly)) {
       // TODO: log
-      return;
+      return false;
     }
 
     QJsonObject manifestJson;
-    manifestJson.insert("name", "easyPlay");  // TODO: use #define
-    manifestJson.insert("description", "EasyPlay device client");
+    manifestJson.insert("name", EXTENSION_NAME);
+    manifestJson.insert("description", MANIFEST_DESCRIPTION);
     manifestJson.insert("path", QDir::cleanPath(clientExePath));
     manifestJson.insert("type", "stdio");
-    manifestJson.insert("allowed_extensions", QJsonArray({"easyPlay@easyPlay"}));  // TODO: use #define
+    manifestJson.insert("allowed_extensions", QJsonArray{{EXTENSION_ID}});
 
     QByteArray manifestContent = QJsonDocument{manifestJson}.toJson(QJsonDocument::Compact);
 
     if (manifestFile.write(manifestContent) < 0) {
       // TODO: log
+      return false;
     }
+
+    return true;
   }
 };
--- software/new-installer/tests/manifestHandlerTest.cpp
+++ software/new-installer/tests/manifestHandlerTest.cpp
@@ -11,44 +11,21 @@
 
 #include "manifestHandler.h"
 
-// TODO: create test class
-
-// #define MANIFEST_ABS_PATH R"(/Users/karl/Library/Application Support/Mozilla/NativeMessagingHosts/easyPlay.json)"
+#define CLIENT_EXE_PATH "directory/file"
 
 TEST(ManifestHandlerTest, CanCreateManifestFile) {
-  // TODO: get home path and don't use abs path
-  std::ifstream manifestFile{"easyPlay.json"};                             // TODO: use #define
-  ManifestHandler::createManifest(QDir::currentPath(), "directory/file");  // TODO: use #define
+  ManifestHandler::createManifest(QDir::currentPath(), CLIENT_EXE_PATH);
 
-  // TODO: use build folder path for the file
+  auto manifestFile = QFile{MANIFEST_FILE_NAME};
+  EXPECT_TRUE(manifestFile.open(QFile::ReadOnly)) << "Couldn't open the manifest file";
 
-  // TODO: check that file can be opened (exists)
+  auto realContent = QString{manifestFile.readAll()};
+  auto expectedContent = QObject::tr(R"({"allowed_extensions":["%1"],"description":"%2","name":"%3","path":"%4","type":"stdio"})")
+                             .arg(EXTENSION_ID,
+                                  MANIFEST_DESCRIPTION,
+                                  EXTENSION_NAME,
+                                  CLIENT_EXE_PATH);
 
-  // TODO: use path set above
-  // std::string expectedContent = {"{\"name\":\"easyPlay\",\"description\":\"EasyPlaydeviceclient\",\"path\":\"directory/file\",\"type\":\"stdio\",\"allowed_extensions\":[\"easyPlay@easyPlay\"]}"};  // TODO: use #define // TODO: remove
-
-  // TODO: use sstream
-  QString expectedContent = QObject::tr("{\"allowed_extensions\":[\"%1\"],\"description\":\"%2\",\"name\":\"%3\",\"path\":\"%4\",\"type\":\"stdio\"}").arg("easyPlay@easyPlay", "EasyPlaydeviceclient", "easyPlay", "directory/file");  // TODO: use #define
-
-  // TODO: rm
-  // auto manifestFile = QFile{"easyPlay.json"};
-  // if (!manifestFile.open(QFile::ReadOnly)) {
-  //   // TODO: log
-  //   return;
-  // }
-
-  // auto realContent = QString{manifestFile.readAll()};
-
-  // realContent.removeIf
-  // TODO; rm
-  // EXPECT_TRUE(expectedContent.compare(realContent) == 0) << "Real content:" << realContent.toStdString() << std::endl
-  //                                                        << "Expected: " << expectedContent.toStdString();
-
-  // EXPECT_STREQ(expectedContent.constData(), realContent.constData()); // TODO: rm
-
-  std::string realContent{std::istreambuf_iterator<char>(manifestFile), std::istreambuf_iterator<char>()};
-  realContent.erase(std::remove_if(realContent.begin(), realContent.end(), isspace), realContent.end());  // TODO: rm
-
-  EXPECT_TRUE(expectedContent.compare(QString::fromStdString(realContent)) == 0) << "Real content:" << realContent << std::endl
-                                                                                 << "Expected: " << expectedContent.toStdString();
+  EXPECT_TRUE(expectedContent.compare(realContent) == 0) << "Real content:" << realContent.toStdString() << std::endl
+                                                         << "Expected: " << expectedContent.toStdString();
 }
