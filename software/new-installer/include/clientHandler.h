480baae031f1d71c4cd87e18427f9437f1801e00
Karl-Heinrich Tamm <karl.heinrich@zoho.com>
2023-07-10 09:11:27 +0300

    check

---
+++ software/new-installer/include/clientHandler.h
@@ -0,0 +1,14 @@
+// TODO: add guard
+#include <QFile>
+#include <QString>
+
+class ClientHandler {
+  static bool copyClientFile(const QString& clientDestPath) {
+    // TODO: add definition to cpp
+    // TODO: add binary file to app as resource
+
+    // TODO: use threads for copy
+
+    QFile::copy("")
+  }
+};
