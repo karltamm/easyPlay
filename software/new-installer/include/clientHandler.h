b4c4fcc900b92bd74a06233feeb7ce8a63b204d2
Karl-Heinrich Tamm <karl.heinrich@zoho.com>
2023-07-10 19:36:05 +0300

    check

--- software/new-installer/include/clientHandler.h
+++ software/new-installer/include/clientHandler.h
@@ -1,14 +1,28 @@
 // TODO: add guard
+#include <QDir>
 #include <QFile>
 #include <QString>
 
 class ClientHandler {
+ public:
   static bool copyClientFile(const QString& clientDestPath) {
     // TODO: add definition to cpp
     // TODO: add binary file to app as resource
 
     // TODO: use threads for copy
+    // TODO: use future for output
 
-    QFile::copy("")
+    // QFile::copy("")
+
+    QFile clientFile{":/easyplay-device-client"};
+
+    qDebug() << "clientFile.size:" << clientFile.size();
+    qDebug() << "clientFile.exists:" << clientFile.exists();
+
+    QDir{}.mkpath(clientDestPath);  // TODO: check for outcome
+
+    clientFile.copy(clientDestPath + "/easyplay-device-client");  // TODO: use #define // TODO: check for outcome
+
+    return true;
   }
 };
--- software/new-installer/tests/clientHandlerTest.cpp
+++ software/new-installer/tests/clientHandlerTest.cpp
@@ -1,5 +1,10 @@
 #include <gtest/gtest.h>
+#include <QDir>
 #include <QFile>
 
+#include "clientHandler.h"
+
 TEST(ClientHandlerTest, CanCopyClientFile) {
+  EXPECT_TRUE(ClientHandler::copyClientFile(QDir::currentPath()));                                      // TODO: add message
+  EXPECT_TRUE(QFile::exists(QDir::currentPath() + "/easyplay-device-client")) << "File wasn't copied";  // TODO: use #define
 }
